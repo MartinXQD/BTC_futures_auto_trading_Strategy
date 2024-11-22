7e3de8764784c625d544a4b8de2992544860db0a
Qidain(Martin) Xing <xing3250@mylaurier.ca>
2024-11-22 16:00:13 -0500

    Update _02_Bollinger_Calculation.py

--- _02_Bollinger_Calculation.py
+++ _02_Bollinger_Calculation.py
@@ -1,19 +1,19 @@
 
+#Construct Bollinger Bands for 1-hour candlestick charts.
 
 import numpy as np
 from _01_Import_Data_Frame import get_data
 
 btc_df = get_data()
-
-# Calculate Bollinger Band
-
-
+# Bollinger Band with 𝑡=2.33， corresponding to a 1% extreme scenario under the assumption of a normal distribution
 def BollingerBand(btc_df, periods=20, t=2.33):
+    #  Define column names for Bollinger Band construction
     key = ['Upper', 'Lower', 'STDEV', 'TyP', 'SMA']
     ub, lb, stdev, price, sma_20 = key
-
+    #Calculate the Typical Price for each entry
     btc_df[price] = btc_df.apply(lambda x: np.mean(
         x[['High', 'Low', 'Close']]), axis=1)
+    
     btc_df[sma_20] = btc_df[price].rolling(periods).mean()
     btc_df[stdev] = btc_df[price].rolling(periods).std()
     btc_df[ub] = btc_df[sma_20] + t * btc_df[stdev]
