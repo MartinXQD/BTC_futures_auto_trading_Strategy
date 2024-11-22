8f9e6269bebcab66b14fff33ad5ed28862090b85
Qidain(Martin) Xing <xing3250@mylaurier.ca>
2024-11-22 18:07:57 -0500

    Update _03_Plot_Bollinger_Band.py

--- _03_Plot_Bollinger_Band.py
+++ _03_Plot_Bollinger_Band.py
@@ -1,18 +1,17 @@
 
+# Visualize dataframe with Bollinger band. 
 import mplfinance as mpf
-
 from _02_Bollinger_Calculation import get_bollinger_data
 
 btc_df = get_bollinger_data()
 
-# plot Bollinger Band
-
 def plot_bollinger_band(btc_df):
+    # Replace time with indexes for tracking
     btc_df.set_index('Time', inplace=True)
-
+    # plot Bollinger Band
     apdict = mpf.make_addplot(btc_df[['SMA', 'Upper', 'Lower']])
-
-    mpf.plot(btc_df, type='candle', addplot=apdict, title='BTC Price with Bollinger Bands',
+    # Visualized BTC/USDT Futures 1-H K line
+    mpf.plot(btc_df, type='candle', addplot=apdict, title='BTCUSDT Futures Price with Bollinger Bands',
              ylabel='Price', volume=False)
 
 
